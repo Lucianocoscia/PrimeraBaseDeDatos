7ca715a9c54ba0dd818fdcd1eca283c3b01e26f2
Luciano Coscia <lucianocoscia5@gmail.com>
2022-12-25 16:24:34 -0300

    segundo Commit descomentando una linea en index

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,7 @@ import Contenedor from "../api.js";
 
 const router = Router();
 
-// const api = new Contenedor("./src/productos.txt");
+const api = new Contenedor("./src/productos.txt");
 
 router.get("/", async (req, res) => {
   res.render("form", { items: await api.getAll() });
